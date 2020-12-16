dca3813a3f1704a52153fc0acea4351b9df8e29f
JIPalliano <ismael_palliano190@hotmail.com>
2020-12-16 11:33:49 -0300

    configurando o Django

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mysite'
+    'website'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

de31975a60a77ba437a6030819a7183778c45f7c
JIPalliano <ismael_palliano190@hotmail.com>
2020-12-16 14:14:53 -0300

    abrindo banco de dados

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mysite'
-    'website'
+    'mysite',
+    'website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
